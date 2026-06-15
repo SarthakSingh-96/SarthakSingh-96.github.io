8efccd536fa0268c415f45879dd4a1b1bb01ecc0
SarthakSingh-96 <sarthaksingh1211@gmail.com>
2026-06-15 15:41:24 -0400

    Fix GitHub Pages root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: (assetInfo) =>
+          assetInfo.name?.slice(-4) === ".css"
+            ? "assets/index.css"
+            : "assets/[name][extname]",
+      },
+    },
+  },
 });
